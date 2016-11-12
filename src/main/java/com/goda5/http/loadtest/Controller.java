edc60ca735003bf3728b88fac613a30bd7d646e8
chentongtc@gmail.com <chentongtc@gmail.com>
2016-11-12 19:42:39 +0000

    first draft

--- src/main/java/com/goda5/http/loadtest/Controller.java
+++ src/main/java/com/goda5/http/loadtest/Controller.java
@@ -1,8 +1,12 @@
 package com.goda5.http.loadtest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-
+    @RequestMapping("/")
+    public ModelAndView home() {
+        return new ModelAndView("home.html", null);
+    }
 }
--- src/main/java/com/goda5/http/loadtest/Main.java
+++ src/main/java/com/goda5/http/loadtest/Main.java
@@ -3,9 +3,11 @@ package com.goda5.http.loadtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
+@EnableWebMvc
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
